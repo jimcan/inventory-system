6d9a9e797826ff94158f976da72d74cdb8dca4e6
Jimboy Cantila <jimcan009@gmail.com>
2020-12-08 10:32:02 +0800

    useFirestore

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -3,18 +3,38 @@ import { firestore } from "../firebase/config";
 
 export function useFirestore(collection) {
   const [docs, setDocs] = useState([])
+  const [filteredDocs, setFilteredDocs] = useState(docs)
+  const ref = firestore.collection(collection)
 
   useEffect(() => {
-    const unsub = firestore.collection(collection)
-      .onSnapshot((snap) => {
-        let documents = []
-        snap.forEach((doc) => {
-          documents.push({ ...doc.data(), id: doc.id })
-        })
-        setDocs(documents)
+    const unsub = ref.onSnapshot((snap) => {
+      let documents = []
+      snap.forEach((doc) => {
+        documents.push({ ...doc.data(), id: doc.id })
       })
+      setDocs(documents)
+      setFilteredDocs(documents)
+    })
     return () => unsub()
   }, [collection])
 
-  return { docs }
+  const filterDocuments = (query, appliedTo) => {
+    setFilteredDocs(docs.filter(x => x[appliedTo].toLowerCase().includes(query)))
+  }
+
+  const addDocument = (data) => {
+    ref.add(data)
+  }
+
+  const updateDocument = (data) => {
+    const id = data.id
+    delete data.id
+    ref.doc(id).update(data)
+  }
+
+  const deleteDocument = (id) => {
+    ref.doc(id).delete()
+  }
+
+  return { filteredDocs, filterDocuments, addDocument, updateDocument, deleteDocument }
 }
